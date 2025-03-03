e02b94b0a3c246258f029f0c8f04020670fc2258
AdityaJoshi-14 <joshiaditya174@gmail.com>
2025-03-03 11:20:42 +0530

    Add files via upload

---
+++ cube_calculator.py
@@ -0,0 +1,10 @@
+# Python program to calculate the cube of a number
+
+# Taking input from the user
+num = float(input("Enter a number: "))
+
+# Calculating the cube
+cube = num ** 3
+
+# Displaying the result
+print(f"The cube of {num} is {cube}")
